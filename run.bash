4460b77ec14c535ff4efc319d82172fc2e67cd63
watson <whxhawk@hotmail.com>
2025-01-06 10:52:08 +0800

    Clean up.

--- run.bash
+++ run.bash
@@ -1,3 +1,9 @@
 sudo docker run --runtime nvidia -it -v share:/share xxxx
-sudo docker run --runtime nvidia -itd -v /tmp/.X11-unix:/tmp/.X11-unix -v /dra/models_ws/BEVDet-ROS-TensorRT:/workspace/BEVDet/ -e DISPLAY=$DISPLAY
+
+sudo docker run --runtime nvidia -itd -v /tmp/.X11-unix:/tmp/.X11-unix -v /dra/models_ws/BEVDet-ROS-TensorRT:/workspace/BEVDet/ -e DISPLAY=$DISPLAY ***:***
+
+sudo docker run --runtime nvidia -e DISPLAY=$DISPLAY -itd -v /tmp/.X11-unix:/tmp/.X11-unix --hostname=$HOSTNAME -v $HOME/.Xauthority:/root/.Xauthority -v /dra/models_ws/BEVDet-ROS-TensorRT:/workspace/BEVDet/ -e DISPLAY=$DISPLAY ***:***
+
 sudo docker exec -it *** /bin/bash
+
+xhost +local:
